9f78eae6482d4846a5dd0a0cc75fd9fdc415e6f7
cesar augusto pimenta <cesar-pimenta@DESKTOP-T2842DS.localdomain>
2022-12-07 11:18:50 -0300

    professionalexperiencia

--- about/views.py
+++ about/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, HttpResponse
+from perfil.models import Profile
 
 
 def index(request):
-    return render(request, 'about/index.html', )
+    profile = Profile.objects.last()
+    return render(request, 'about/index.html', context={
+        "profile": profile,
+    })

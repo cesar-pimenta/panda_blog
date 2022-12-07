3b8cc9af79981d1b8b490ad658c8d33bc6c1df84
cesar augusto pimenta <cesar-pimenta@DESKTOP-T2842DS.localdomain>
2022-12-07 14:19:19 -0300

    education-and-certification-info

--- about/admin.py
+++ about/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
-from about.models import ProfessionalExperience
+from about.models import ProfessionalExperience, ProfileEducation, ProfileCertification
+
+
 # Register your models here.
 
 
 @admin.register(ProfessionalExperience)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'company', 'position', )
+    list_display = ('user', 'company', 'position',)
+
+
+@admin.register(ProfileEducation)
+class EducationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'institution', 'course',)
+
+
+@admin.register(ProfileCertification)
+class CertificationAdmin(admin.ModelAdmin):
+    list_display = ('user', "institution", 'course',)
--- about/models.py
+++ about/models.py
@@ -18,3 +18,35 @@ class ProfessionalExperience(DateCreateUpdate):
 
     def __str__(self):
         return '{} - in -> {}'.format(self.position, self.company)
+
+
+class ProfileEducation(DateCreateUpdate):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='education')
+    institution = models.CharField(max_length=256, null=True, blank=True)
+    course = models.CharField(max_length=256, null=True, blank=True)
+    start_date = models.DateField(null=True, blank=True)
+    final_date = models.DateField(null=True, blank=True)
+    resume = models.TextField(null=True, blank=True)
+    is_studying = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Education'
+        verbose_name_plural = 'Educations'
+
+    def __str__(self):
+        return '{} - in -> {}'.format(self.course, self.institution)
+
+
+class ProfileCertification(DateCreateUpdate):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='certification')
+    institution = models.CharField(max_length=256, null=True, blank=True)
+    course = models.CharField(max_length=256, null=True, blank=True)
+    year = models.DateField(null=True, blank=True)
+    resume = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Certification'
+        verbose_name_plural = 'Certifications'
+
+    def __str__(self):
+        return '{} - in -> {}'.format(self.course, self.institution)
